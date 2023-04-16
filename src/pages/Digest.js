421d9de2d8528af315ef1b53d40c8850a40794bc
Karina Lam <karinalamkll@gmail.com>
2023-04-15 21:44:20 -0400

    Structure of Digest page

--- src/pages/Digest.js
+++ src/pages/Digest.js
@@ -4,8 +4,50 @@ import './Digest.css'
 function Digest() {
 
     return (
-        <div className='nav-padding'>
-            Digest
+        <div className='nav-padding digest-container'>
+            <div className="top-digest-bar"> 
+            <h2>Weekly Digest</h2>
+            </div>
+
+            <div className="center digest">
+                <span className="date-bar">
+                    <span className="back-btn">&lt;</span>
+                    <span className="date-range">
+                    Apr 15 - Apr 22
+                    </span>
+                    <span className="fwd-btn">&gt;</span>
+                </span>
+                <hr class="line"></hr>
+
+                <div className="digest-gem-stats">
+                    Hey Joni, <br></br> You learned 56 blocks 
+                    <span className="digest-gem-perc">
+                        30%
+                    </span>
+                    this week! <br></br>
+                </div>
+
+                <div className="digest-motivation">
+                    Some chatgtp motivational quote
+                </div>
+
+                <div className="digest-top-tags">
+                    Top Tags
+                    <ol type="1">
+                        <li>Java</li>
+                        <li>Docker</li>
+                        <li>Spring Boot</li>
+                    </ol>
+                </div>
+
+                <div className="digest-suggestions">
+                    Something that may interest you...
+                    <div className="interest-tags">
+                    
+                    </div>
+                </div>
+
+            </div>
         </div>
     )
 }
