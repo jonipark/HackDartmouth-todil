5ecab95bf9ebdfef360af1f83e36c008ec16d313
Karina Lam <karinalamkll@gmail.com>
2023-04-15 22:17:03 -0400

    early stage of explore

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -4,8 +4,15 @@ import './Explore.css'
 function Explore() {
 
     return (
-        <div className='nav-padding'>
-            Explore
+        <div className='nav-padding digest-container'>
+            <div className="top-explore-bar"> 
+            <h2>Explore</h2>
+            </div>
+
+            <div className="center explore">
+                
+
+            </div>
         </div>
     )
 }
